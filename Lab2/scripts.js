417ef55be397ebf10af2919eee0ad15fdf7ec10e
konrad.strek <konrad.strek@gmail.com>
2021-03-28 15:18:54 +0200

    4.0

--- Lab2/scripts.js
+++ Lab2/scripts.js
@@ -3,6 +3,8 @@
 var toDoList;
 var toDoText;
 
+var trash = null;
+
 function onLoad()
 {
 	toDoList = document.getElementById("toDoList");
@@ -10,13 +12,53 @@ function onLoad()
 	
 }
 
-function toggleGray(element)
+$(document).ready(function(){
+	$(".open").on("click", function() {	
+		$(".popup-overlay, .popup-content").addClass("active");
+	});
+
+	$(".closeYes").on("click", function() {
+		$(".popup-overlay, .popup-content").removeClass("active");
+		trash = $(".deleteListElement").parent().get(0);
+		$(".deleteListElement").parent().remove();
+		$(".undoButton").prop("disabled", false);
+	});
+	
+	$(".closeNo").on("click", function() {
+		$(".popup-overlay, .popup-content").removeClass("active");
+		$(".deleteListElement").removeClass("deleteListElement");
+	});
+	
+	$(".undoButton").prop("disabled", true);
+	
+	$(".undoButton").on("click", function() {
+		if(trash != null)
+		{		
+			let delButton = $(trash).find(".delete-button").get(0);
+			console.log(delButton);
+	
+			toDoList.appendChild(trash);
+			$(delButton).on("click", function() {	
+				$(".popup-overlay, .popup-content").addClass("active");
+				$(delButton).addClass("deleteListElement");
+			});
+			$(delButton).removeClass("deleteListElement");
+			
+			$(".undoButton").prop("disabled", true);
+			
+			trash = null;
+		}
+	});
+})	
+
+
+function crossOut(element)
 {
 	console.log("toggray");
 	let textPart = element.querySelector("span[name='textPart']");
 	let datePart = element.querySelector("span[name='datePart']");
 	
-	if(element.getAttribute("checked") == "false")
+	if(element.getAttribute("checked") === "false")
 	{
 		textPart.style.color = "DarkGrey";
 		textPart.style.setProperty("text-decoration", "line-through")
@@ -43,15 +85,15 @@ function ListElement(value)
 
 function addToList()
 {
-	if(toDoText.value == "")
+	if(toDoText.value === "")
 		return;
 
 	let htmlListItem = document.createElement("li");
-	htmlListItem.setAttribute("onclick", "toggleGray(this)");
 
 	let textPart = document.createElement("span");
 	textPart.setAttribute("name", "textPart");
 	textPart.appendChild(document.createTextNode(toDoText.value));
+	textPart.setAttribute("onclick", "crossOut(this.parentElement)");
 	
 	let datePart = document.createElement("span");
 	datePart.setAttribute("name", "datePart");
@@ -59,9 +101,8 @@ function addToList()
 	let deleteButton = document.createElement("button");
 	deleteButton.setAttribute("type", "button");
 	deleteButton.innerHTML = "Usuń";
-	deleteButton.setAttribute("class", ".btn-danger");
-	deleteButton.setAttribute("style", "background-color: red; color: white");
-	deleteButton.setAttribute("onclick", "deleteListElement(this)");
+	deleteButton.setAttribute("class", ".btn-danger open");
+	deleteButton.classList.add("delete-button");
 
 	htmlListItem.appendChild(textPart);
 	htmlListItem.innerHTML += " ";
@@ -71,10 +112,9 @@ function addToList()
 	htmlListItem.setAttribute("checked", "false");
 	
 	toDoList.appendChild(htmlListItem);
-}
-
-
-function deleteListElement(button)
-{
-	$(button).parent().remove();
+	
+	$(deleteButton).on("click", function() {	
+		$(".popup-overlay, .popup-content").addClass("active");
+		$(deleteButton).addClass("deleteListElement");
+	});
 }
